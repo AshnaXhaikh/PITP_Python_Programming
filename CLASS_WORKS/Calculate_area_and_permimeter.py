999cc5ad21b7df55e0ee533b5753164265db8b1e
Ashna Imtiaz <ashnaxhaikh@gmail.com>
2025-09-27 13:19:44 +0200

    Create Calculate_area_and_permimeter.py

---
+++ CLASS_WORKS/Calculate_area_and_permimeter.py
@@ -0,0 +1,10 @@
+# Calculate the area
+"""take length and width as input"""
+length = float(input("enter length: "))
+width = float(input("enter width: "))
+
+area = length * width 
+perimeter = 2*(length + width)
+
+print(f"The area of rectangle = {area}cm^2")
+print(f"Perimeter of rectangle = {perimeter}")
